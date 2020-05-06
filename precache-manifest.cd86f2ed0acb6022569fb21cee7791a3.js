f050bdfc66675c6d883f2c7160e2fa38dff7be44
Roshan Kanwar <roshan.18je0696@am.iitism.ac.in>
2020-05-07 04:09:59 +0530

    Updates

---
+++ precache-manifest.cd86f2ed0acb6022569fb21cee7791a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb3593544cf680b90645e0aadfdcfd8e",
+    "url": "/robo_friends/index.html"
+  },
+  {
+    "revision": "b97aa287f9e81a575cb9",
+    "url": "/robo_friends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "3645866ff3813545860f",
+    "url": "/robo_friends/static/css/main.7bbbc36d.chunk.css"
+  },
+  {
+    "revision": "b97aa287f9e81a575cb9",
+    "url": "/robo_friends/static/js/2.22b1ca94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo_friends/static/js/2.22b1ca94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3645866ff3813545860f",
+    "url": "/robo_friends/static/js/main.a24ee49c.chunk.js"
+  },
+  {
+    "revision": "22addf4de597ed68f4b4",
+    "url": "/robo_friends/static/js/runtime-main.9490953b.js"
+  }
+]);
